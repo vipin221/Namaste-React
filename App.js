f0a3bb4b2db0d1b3b0233d54aa72abf4b90af2f8
Vipin Maurya <mauryavipin30@gmail.com>
2024-01-11 20:26:01 +0530

    react app created from scratch

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/**
- * ex1 ->>
- * <div id="parent">
- *       <div id="child">
- *           <h1>hello Im H1 tag</h1>
- *      </div>
- * </div>
- */
-
-// creating like above
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 const parent = React.createElement(
   "div",
@@ -19,15 +11,7 @@ const parent = React.createElement(
   )
 );
 
-/* ex2 -->>
- * <div id="parent">
- *       <div id="child">
- *           <h1>Hello Im H1 tag</h1>
- *           <h1> Hello Im sibling of H1 tag</h1>
- *      </div>
- * </div>
- *
- */
+
 const parent2 = React.createElement(
   "div",
   { id: "parent" },
@@ -35,11 +19,9 @@ const parent2 = React.createElement(
     "div",
     { id: "child" },
     [React.createElement("h2", {}, "hello Im Inside H2 Tag"), React.createElement("h1", {}, "Sibling of H2")] 
-    // for creating sibling element pass it like array
+
   )
 );
-
-// const heading = React.createElement("h1", {}, "Hello World From React!!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const root1 = ReactDOM.createRoot(document.getElementById("root1"));
