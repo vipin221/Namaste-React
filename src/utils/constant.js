ca19d0625f58297c46618dee54d2e0258ad0c0d0
Vipin Maurya <mauryavipin30@gmail.com>
2024-05-18 20:30:04 +0530

    Updated

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,5 @@
 export const LOGO_URL = "https://w1.pngwing.com/pngs/188/805/png-transparent-food-icon-tea-cooking-drink-kitchen-fast-food-logo-restaurant.png";
 export const RES_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-// export const RES_LISTS = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId="
 export const Banner_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
